d9d1ff9c10f0d836be20d9072b5a07e7a422d3fd
CUONGNT <CUONGNT@DESKTOP-2M98BF7>
2021-04-22 20:17:32 +0700

    commit

--- LanguageCenter/Areas/Home/Controllers/HomeController.cs
+++ LanguageCenter/Areas/Home/Controllers/HomeController.cs
@@ -19,6 +19,8 @@ namespace LanguageCenter.Areas.Home.Controllers
         public HomeController()
         {
             _ClassRepository = new ClassRepository();
+
+
         }
         public ActionResult Index()
         {
