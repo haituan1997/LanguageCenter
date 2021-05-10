605f7d2eed3769601530ff7294b4401a4eeec324
haituan <haituan@edufree.win>
2021-05-10 21:23:30 +0700

    commit

--- LanguageCenter/Areas/Home/Controllers/HomeClassWeekDayController.cs
+++ LanguageCenter/Areas/Home/Controllers/HomeClassWeekDayController.cs
@@ -1,9 +1,13 @@
-﻿using LanguageCenter.Repository;
+﻿using LanguageCenter.DataLayer.Object;
+using LanguageCenter.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LanguageCenter.Areas.Home.Models.Class;
+using AutoMapper;
+using LanguageCenter.Layer.DataLayer.Object;
 
 namespace LanguageCenter.Areas.Home.Controllers
 {
@@ -15,6 +19,7 @@ namespace LanguageCenter.Areas.Home.Controllers
         {
             _classWeekDayRepository = new ClassWeekDayRepository();
             _classRepository = new ClassRepository();
+            Mapper.CreateMap<Class, ClassModel>();
         }
         // GET: Home/HomeClassWeekDay
         public ActionResult ClassWeekDay(int? index)
@@ -24,16 +29,25 @@ namespace LanguageCenter.Areas.Home.Controllers
             ViewBag.Classes = datas;
             return View("ClassWeekDay");
         }
-        public ActionResult FullCalendar(int? classID)
+        public ActionResult FullCalendar(long? classID)
         {
-            //var classes = _classRepository.Get_AllClasses();
-            //int total;
-            //if (classID != null)
-            //{
-            //    var data = _classWeekDayRepository.Get_ClassWeekDayByClassID((int)classID, out total, 1, 999999, "", "");
-            //}
-            ViewBag.ClassID = classID;
-            return View("FullCalendar");
+            int total;
+            if (classID != null)
+            {
+                var data = _classWeekDayRepository.Get_ClassWeekDayByClassID((int)classID, out total, 1, 999999, "", "");
+            }
+            var model = Mapper.Map<Class, ClassModel>(  _classRepository.Get_ClassByClassID((long)classID));
+            return View("FullCalendar", model);
+        }
+        public ActionResult GetCalendarByClassID(long? classID)
+        {
+            int total = 0;
+            var datas = new List<ClassWeekDay>();
+            if (classID != null)
+            {
+                datas = _classWeekDayRepository.Get_ClassWeekDayByClassID((int)classID, out total, 1, 999999, "", "").ToList();
+            }
+            return Json(new { success = true, data = datas }, JsonRequestBehavior.AllowGet);
         }
     }
 }
