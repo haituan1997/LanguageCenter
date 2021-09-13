e4d9cf52247374a422057cfd9aeed5bb05128f7a
TuanDhh <tuandhh@buca>
2021-09-13 17:19:15 +0700

    add object filter

---
+++ LanguageCenter/Layer/DataLayer/Object/ObjectFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageCenter.Layer.DataLayer.Object
+{
+    public class ObjectFilter
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string SearchBy { get; set; }
+        public string OrderBy { get; set; }
+    }
+}
